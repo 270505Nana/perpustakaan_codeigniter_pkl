64f7a20a85c2fffee9cb7adacafd017e31bf5e65
nana <prajna270505@gmail.com>
2022-04-27 08:31:29 +0700

    Update menerapkan sb admin dan membuat halaman login

---
+++ application/controllers/Login.php
@@ -0,0 +1,43 @@
+<?php
+
+class Login extends CI_Controller{
+
+    public function __construct(){
+
+        parent::__construct();
+        $this->load->model('My_Models');
+    }
+
+    public function index(){
+        $this->load->view('halaman_login');
+    }
+
+    public function do_login(){
+        // memasukkan nama dari form yang di input
+        $user = $this->input->post('uname');
+        $pass = $this->input->post('password');
+
+        // var login untuk mengakses ke model
+        // parameter dta yang dikirim $user dan $pass
+        $login = $this->My_Models->Cek_login($user,$pass);
+
+        // ketika ada datanya (>0) maka akan menampilkan berhasil
+        // kalau ga ada akan muncul gagal, ex: username dan password terdaftar jadi ada datanya, tapi kalau belum terdaftar akan gagal.
+
+        // jika datanya ada akan di arahkan ke halaman admin
+        if(count($login) > 0 ){
+           
+           $this->load->view('admin/admin');
+        }else{
+            $this->session->set_flashdata('login','Periksa Username dan Password Anda');
+            redirect('login/index');
+        }
+
+    }
+
+    public function admin(){
+        $this->load->view('admin/admin');
+    }
+
+}
+?>
---
+++ application/models/My_Models.php
@@ -0,0 +1,15 @@
+<?php
+
+class My_Models extends CI_Model{
+
+    public function Cek_login($user,$pass){
+    // menerima 2 parameter
+    return $this->db->get_where('admin', array(
+        'username' => $user,
+        'password' => $pass
+    ))->result_array();
+
+    }
+}
+
+?>
